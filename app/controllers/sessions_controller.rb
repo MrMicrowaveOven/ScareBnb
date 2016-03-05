eafd537fc18c216badc5a08528654c13a530548b
Benjamin <benj@zagorski.com>
2016-03-05 13:34:15 -0800

    FreezeBugSolved

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,6 @@ class SessionsController < ApplicationController
   end
 
   def create
-    debugger
     user = User.find_by_credentials(
       params[:user][:email],
       params[:user][:password]
